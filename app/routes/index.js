784ab72e28ad3296ef3993d321bab84f1204fe3b
Frangia22 <giacherofranc22@gmail.com>
2023-07-19 14:03:23 -0300

    Feat: routes add product anda category

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const bcrypt = require('bcrypt');
 const apiUsers = require('../api/users');
+const apiProducts = require('../api/products');
 const { isAuthenticated, isAdmin } = require('../controllers/auth');
 var router = express.Router();
 let user;
@@ -13,17 +14,33 @@ router.get('/', function(req, res, next) {
 });
 router.get('/addProduct', isAuthenticated, isAdmin, async(req, res) => {
   user = req.user;
-  res.render('pages/add', {title: 'Agregar producto', user});
+  const categorys = await apiProducts.getCategory();
+  res.render('pages/add', {title: 'Agregar producto', user, categorys});
 });
-/*
 router.post('/addproduct', isAuthenticated, isAdmin, async(req, res) => {
-  
-});*/
+  user = req.user;
+  const { name, price, image, details, linkPago, otherDetails, categorias } = req.body;
+  console.log(categorias);
+  await apiProducts.addProduct(name, price, image, details, linkPago, otherDetails);
+  const product = await apiProducts.getProductByName(name);
+  await product.addCategorys(categorias);
+  res.redirect('/');
+});
 router.get('/addService', isAuthenticated, isAdmin, async(req, res) => {
   user = req.user;
   res.render('pages/add', {title: 'Agregar servicio', user});
 });
-
+router.get('/addCategory', isAuthenticated, isAdmin, async(req, res) => {
+  user = req.user;
+  res.render('pages/add', {title: 'Agregar categoria', user});
+});
+router.post('/addcategory', isAuthenticated, isAdmin, async(req, res) => {
+  user = req.user;
+  const { name } = req.body;
+  console.log(name);
+  await apiProducts.addCategory(name);
+  res.redirect('/');
+})
 
 router.get('/products', isAuthenticated, isAdmin, (req, res) => {
   res.send('Esta es la pagina de productos');
